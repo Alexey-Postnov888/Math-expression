b0d70c63bbc9c8c0f3213207c01013d3683fdefb
Alexey Postnov <adpostnov@yandex.ru>
2024-05-12 22:09:06 +0500

    lab6

---
+++ RpnConsoleApp/RpnConsole.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using RpnLogic;
+
+namespace RpnConsoleApp
+{
+    class RpnConsole
+    {
+        class RPN
+        {
+            static string GetInput()
+            {
+                Console.Write("Ваше выражение: ");
+                return Console.ReadLine();
+            }
+            static void Main(string[] args)
+            {
+                string input = GetInput().Replace(" ", string.Empty);
+                RpnCalculator rpn = new RpnCalculator(input);
+                double result = rpn.Result;
+
+                // Console.WriteLine($"Ваше выражение в ОПЗ: {string.Join(" ", toRPN(tokens))}"); //// to do normal output
+                Console.WriteLine($"Результат: {result}");
+
+            }
+        }
+    }
+}
